593c8480fb58affa8983d80eaa8cff6545d824de
lizhihao <310645359@qq.com>
2020-12-08 12:26:27 +0800

    create

---
+++ main.c
@@ -0,0 +1,24 @@
+//the coding is GB2312
+#include<stdio.h>
+
+int main()
+{
+    puts("▼杭电");
+    puts("  〉理学院");
+    puts("  〉机械学院");
+    puts("  〉自动化学院");
+    puts("  ▼卓越学院");
+    puts("      〉19184111");
+    puts("      〉19184112");
+    puts("      〉19184113");
+    puts("      〉19184114");
+    puts("      ▼19184115");
+    puts("          张三");
+    puts("          李四");
+    puts("          王五");
+    puts("  〉材环学院");
+    puts("  〉计算机学院");
+    puts("  〉经济学院");
+    puts("  〉人艺数法");
+    return 0;
+}
